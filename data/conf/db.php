fb762daea58dddaf1ac48dfc22608d3658eac50a
unknown <gewanbo@gmail.com>
2015-05-16 15:50:52 +0800

    Add data folder.

---
+++ data/conf/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'db',
+    'DB_USER' => 'root',
+    'DB_PWD' => '1234567890',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'sp_',
+    //密钥
+    "AUTHCODE" => '9YOrMpXKerbFwZly5s',
+    //cookies
+    "COOKIE_PREFIX" => 'jBMG60_',
+);
+?>
